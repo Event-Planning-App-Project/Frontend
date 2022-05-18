1320233fb139ee926f6cfcb23690a7df733511a5
fathonio <fathoniahmad1347@gmail.com>
2022-05-18 14:19:20 +0700

    pembenaran slicing detailEvent

--- pages/detailEvent/[id].jsx
+++ pages/detailEvent/[id].jsx
@@ -3,18 +3,16 @@ import Image from "next/image";
 
 function DetailEvent() {
   return (
-    // detail event
-    <div className="d-flex px-5 py-3">
-      <div className="me-5">
+    <div className="d-flex justify-content-center px-5 py-3">
+      <div className="me-3">
         <Card>
           <Card.Img variant="top" />
           <Image
             src="/event-cover.svg"
             alt="Event-cover"
-            width={350}
+            width={900}
             height={350}
           />
-
           <Card.Body>
             <Card.Title>Detail Event</Card.Title>
             <Card.Text>
@@ -27,89 +25,67 @@ function DetailEvent() {
           </Card.Body>
         </Card>
 
-        {/* review */}
-        <div className="pt-3">
-          <Card>
-            <Card.Header as="h5">Drop your Review!</Card.Header>
-            <Card.Body className="d-flex">
-              <div className="me-3">
-                <Image
-                  src="/user-logo.svg"
-                  alt="user-logo"
-                  width={50}
-                  height={50}
-                />
-              </div>
-              <div>
-                <Card.Title>username</Card.Title>
-                <Card.Text>
-                  Please add your content here. Keep it short and simple. And
-                  smile
-                </Card.Text>
-              </div>
-            </Card.Body>
-          </Card>
-        </div>
-
-        {/* creat review */}
-        <div className="py-3">
-          <Card>
-            <Card.Body className="d-flex">
-              <div className="me-3">
-                <Image
-                  src="/user-logo.svg"
-                  alt="user-logo"
-                  width={50}
-                  height={50}
-                />
-              </div>
-              <div>
-                <Card.Title>username</Card.Title>
-                <Card.Text>
-                  <Form.Group
-                    className="d-grid gap-2"
-                    controlId="formBasicEmail"
-                  >
-                    <Form.Control
-                      autoComplete="of"
-                      type="text"
-                      placeholder=""
-                    />
-                  </Form.Group>
-                </Card.Text>
-              </div>
-            </Card.Body>
-          </Card>
-        </div>
-
-        <Button variant="outline-danger">Kirim</Button>
-      </div>
-
-      {/* date */}
-      <div>
-        <Card style={{ width: "25rem" }}>
-          <Card.Body className="my-3">
-            <Card.Title className=" mb-5">
-              Design 101: Creating beautiful and intuitive UI/UX as a
-              non-designer
+        <Card className="my-4">
+          <Card.Header as="h5">Drop your Review!</Card.Header>
+          <Card.Body className="d-flex">
+            <Card.Title className="me-4">
+              <Image
+                src="/user-logo.svg"
+                alt="user-logo"
+                width={50}
+                height={50}
+              />
             </Card.Title>
-            <Card.Text>thursday, 19 May 2022</Card.Text>
-            <Card.Text>18:00 PM</Card.Text>
-            <Card.Text>online</Card.Text>
+            <Card.Title>
+              <Card.Title>username</Card.Title>
+              <Card.Text>
+                Please add your content here. Keep it short and simple. And
+                smile
+              </Card.Text>
+            </Card.Title>
           </Card.Body>
         </Card>
 
-        {/* harga tiket */}
-        <Card className="my-4" style={{ width: "25rem" }}>
-          <Card.Body>
-            <div className="d-flex my-3">
-              <Card.Text>harga tiket</Card.Text>
-              <Card.Text className="ps-5 ms-5">Rp. 350.000</Card.Text>
-            </div>
-            <Button className="mt-2" variant="danger" size="sm">
-              beli tiket
-            </Button>
+        <Card className="mb-4">
+          <Card.Body className="d-flex">
+            <Card.Title className="me-4">
+              <Image
+                src="/user-logo.svg"
+                alt="user-logo"
+                width={50}
+                height={50}
+              />
+            </Card.Title>
+            <Card.Title>
+              <Card.Title>UserName</Card.Title>
+              <Card.Title>
+                <Form.Group className="d-grid gap-2" controlId="formBasicEmail">
+                  <Form.Control autoComplete="of" type="text" placeholder="" />
+                </Form.Group>
+              </Card.Title>
+            </Card.Title>
           </Card.Body>
+        </Card>
+        <Button variant="outline-danger">Kirim</Button>
+      </div>
+
+      <div className="ms-3">
+        <Card>
+          <Card.Title>
+            Design: Creating beatiful and intuitive UI/UX as o non-designer
+          </Card.Title>
+          <Card.Text>thursday, 19 May 2022</Card.Text>
+          <Card.Text>18:00 PM</Card.Text>
+          <Card.Text>online</Card.Text>
+        </Card>
+        <Card className="mt-4">
+          <Card.Body className="d-flex justify-content-center">
+            <Card.Title className="me-5">Harga Total</Card.Title>
+            <Card.Title>Rp.350.000</Card.Title>
+          </Card.Body>
+          <Button className="mt-2" variant="danger" size="sm">
+            beli tiket
+          </Button>
         </Card>
       </div>
     </div>
